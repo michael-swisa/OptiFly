269f270f1f8926a59543ee597de45d4c3886dc98
Michael Swisa <ms0504109182@gmail.com>
2024-09-12 11:57:49 +0300

    add get_weather in request

--- packege/request.py
+++ packege/request.py
@@ -4,3 +4,11 @@ def get_location(cuantry, key):
     url_get_location = rf'https://api.openweathermap.org/geo/1.0/direct?q={cuantry}&appid={key}'
     location = requests.get(url_get_location).json()
     return location
+
+def get_weather(cuantry, key):
+    url_get_weather = rf'https://api.openweathermap.org/data/2.5/forecast?q={cuantry}&appid={key}'
+    weather = requests.get(url_get_weather).json()
+    return weather
+
+
+print(get_weather('yemen', 'a10e75d5c94c635e8496808e1a2918a5'))
